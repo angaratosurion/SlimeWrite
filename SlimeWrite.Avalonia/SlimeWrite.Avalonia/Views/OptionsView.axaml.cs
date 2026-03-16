248fb3d0f7888b97ee66dfe491eaa9f47e8cb8ff
angaratosurion <angarato.surion@gmail.com>
2026-03-16 12:37:34 +0200

    a
    deleted a useless fodler
    added options dialog

---
+++ SlimeWrite.Avalonia/SlimeWrite.Avalonia/Views/OptionsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SlimeWrite.Avalonia.Views;
+
+public partial class OptionsView : Window
+{
+    public OptionsView()
+    {
+        InitializeComponent();
+    }
+}
