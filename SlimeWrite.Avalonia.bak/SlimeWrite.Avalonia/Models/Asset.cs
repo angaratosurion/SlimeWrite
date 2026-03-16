755f8183e978778f728763b0f777fe12740d48f5
angaratosurion <angarato.surion@gmail.com>
2026-03-16 12:02:41 +0200

    fdf

---
+++ SlimeWrite.Avalonia.bak/SlimeWrite.Avalonia/Models/Asset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimeWrite.Avalonia.Models
+{
+    public class Asset
+    {
+        public string name { get; set; }
+        public string browser_download_url { get; set; }
+    }
+}
---
+++ SlimeWrite.Avalonia.bak/SlimeWrite.Avalonia/Models/GitHubRelease.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlimeWrite.Avalonia.Models
+{
+    public class GitHubRelease
+    {
+        public string tag_name { get; set; }
+        public Asset[] assets { get; set; }
+    }
+}
