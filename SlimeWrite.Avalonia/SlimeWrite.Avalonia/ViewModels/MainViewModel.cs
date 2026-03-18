b1fb846dd31bfa802a3462928e82e595ba74cd4d
angaratosurion <angarato.surion@gmail.com>
2026-03-18 10:25:28 +0200

    a
    aa

--- SlimeWrite.Avalonia/SlimeWrite.Avalonia/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace SlimeWrite.Avalonia.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
-}
